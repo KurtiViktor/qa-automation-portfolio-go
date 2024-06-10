0430ebbb2c75fb3dc148621a7d2226536756e2fa
Viktor Kurti <aplikat@ya.ru>
2024-06-10 18:05:55 +0200

    refactor(test-and-readme.md): fix hello test
    add empty readme.md

--- hello_test.go
+++ hello_test.go
@@ -5,13 +5,13 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
-		want := "Hello, Chris!"
+		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, World!"
+		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
